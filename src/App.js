0522bce2f85f61067188101c5616518fa176aa3b
2imad <imadyoussoufi@gmail.com>
2019-09-21 12:22:18 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "typeface-roboto";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("https://api.randomuser.me/?results=20");
+      const users = await res.json();
+      const parsed =
+        (await users) &&
+        users.results.map(user => ({
+          name: `${user.name.first} ${user.name.last}`,
+          email: user.email,
+          thumbnail: user.picture.thumbnail
+        }));
+      setContacts(parsed);
+    };
+    getData();
+  }, []);
+
+  return <div> {contacts.length} </div>;
+};
 
 export default App;
